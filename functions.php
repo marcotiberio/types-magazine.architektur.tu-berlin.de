a6af699e2c2728337559c5bfe4714b9ae9f307ac
Marco <marco@defrostudio.eu>
2020-07-12 15:12:53 +0200

    types script js

--- functions.php
+++ functions.php
@@ -146,7 +146,12 @@ function types_magazine_scripts() {
 	wp_enqueue_style( 'types-magazine-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'types-magazine-style', 'rtl', 'replace' );
 
+	wp_deregister_script( 'jquery' );
+	wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js', array(), null, true);
+
 	wp_enqueue_script( 'types-magazine-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+
+	wp_enqueue_script('tu-berlin-script', get_template_directory_uri() . '/js/script.js', array( 'jquery' ), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
---
+++ js/script.js
@@ -0,0 +1,114 @@
+// Types
+$(document).ready(function(){
+    $( "#arrowsTypes" ).click(function() {
+        $( "#boxTypes" ).animate({
+            height: "65vh", 
+            }, 200 );
+        });
+    });
+    
+    $(document).ready(function(){
+    $( "#arrowsTypes" ).click(function() {
+        $( "#arrowOne" ).animate({
+            margin: "0 100px", 
+            }, 200 );
+        });
+    });
+    
+    $(document).ready(function(){
+    $( "#arrowsTypes" ).click(function() {
+        $( "#arrowTwo" ).animate({
+            margin: "0 100px", 
+            }, 200 );
+        });
+    });
+    
+    $(document).ready(function(){
+    $( "#arrowsTypes" ).click(function() {
+        $( "#arrowThree" ).animate({
+            margin: "0 100px", 
+            }, 200 );
+        });
+    });
+
+    $(document).ready(function(){
+    $( "#arrowsTypes" ).click(function() {
+        $( "#infoTypes" ).animate({
+            margin: "5% 0 0 0", 
+            }, 200 );
+        });
+    });
+
+    $(document).ready(function(){
+    $( "#arrowsTypes" ).click(function() {
+        $( "#knowledgeTypes" ).animate({
+            margin: "-300px 0 0 0", 
+            }, 200 );
+        });
+    });
+    
+    $(document).ready(function(){
+    $( "#titleTypes" ).click(function() {
+        $( "#boxTypes" ).animate({
+            height: "12vh", 
+            }, 200 );
+        });
+    });
+    
+    $(document).ready(function(){
+    $( "#titleTypes" ).click(function() {
+        $( "#arrowOne" ).animate({
+            margin: "0", 
+            }, 200 );
+        });
+    });
+    
+    $(document).ready(function(){
+    $( "#titleTypes" ).click(function() {
+        $( "#arrowTwo" ).animate({
+            margin: "0", 
+            }, 200 );
+        });
+    });
+    
+    $(document).ready(function(){
+    $( "#titleTypes" ).click(function() {
+        $( "#arrowThree" ).animate({
+            margin: "0", 
+            }, 200 );
+        });
+    });
+
+    $(document).ready(function(){
+    $( "#titleTypes" ).click(function() {
+        $( "#infoTypes" ).animate({
+            margin: "2% 0 0 0", 
+            }, 200 );
+        });
+    });
+
+    $(document).ready(function(){
+    $( "#titleTypes" ).click(function() {
+        $( "#knowledgeTypes" ).animate({
+            margin: "0 0 0 0", 
+            }, 200 );
+        });
+    });
+
+
+
+$(document).ready(function(){
+    $( "#arrowsTypesMobile" ).click(function() {
+        $( "#boxTypesMobile" ).animate({
+            height: "100vh", 
+            }, 200 );
+        });
+    });
+
+$(document).ready(function(){
+    $( "#titleTypesMobile" ).click(function() {
+        $( "#boxTypesMobile" ).animate({
+            height: "12vh", 
+        }, 200 );
+    });
+});
