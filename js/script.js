96acdff95a8054cfd0e13e85467ff57c33172da2
Marco <marco@defrostudio.eu>
2020-07-14 11:55:01 +0200

    margin script

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
     $(document).ready(function(){
     $( "#arrowsTypes" ).click(function() {
         $( "#arrowOne" ).animate({
-            margin: "0 50px", 
+            margin: "0 65px", 
             }, 200 );
         });
     });
@@ -26,7 +26,7 @@ $(document).ready(function(){
     $(document).ready(function(){
     $( "#arrowsTypes" ).click(function() {
         $( "#arrowTwo" ).animate({
-            margin: "0 50px", 
+            margin: "0 65px", 
             }, 200 );
         });
     });
@@ -34,7 +34,7 @@ $(document).ready(function(){
     $(document).ready(function(){
     $( "#arrowsTypes" ).click(function() {
         $( "#arrowThree" ).animate({
-            margin: "0 50px", 
+            margin: "0 65px", 
             }, 200 );
         });
     });
