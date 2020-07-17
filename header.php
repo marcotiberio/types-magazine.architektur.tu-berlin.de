fee022104ecdce533fb65aa6fd6404973e8cdd4a
Marco <marco@defrostudio.eu>
2020-07-17 16:29:19 +0200

    test acf

--- header.php
+++ header.php
@@ -63,11 +63,7 @@
 				<h1>of Knowledge Production</h1>
 			</div>
 			<div id="infoTypes">
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
-				et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
-				aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
-				dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
-				officia deserunt mollit anim id est laborum.</p>
+				<p><?php the_field('text_field'); ?></p>
 			</div>
 		</div>
 
